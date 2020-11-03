c75445be22406704e71305c6289d03bb39f514a7
Sv9topolk <sv9topolk@mail.ru>
2020-11-03 13:28:11 +0300

    added desktop menu

---
+++ #src/js/main.js
@@ -0,0 +1,8 @@
+
+const menuToggle = document.querySelector('#menu-toggle');
+const menuHide = document.querySelector('#menu-hide');
+
+menuToggle.onclick = function () {
+	menuToggle.classList.toggle('burger--active');
+	menuHide.classList.toggle('menu--hide');
+}
