8542ed2adbcaab2237d82e3be2649ecbfc7e9295
Sv9topolk <sv9topolk@mail.ru>
2020-11-07 16:37:42 +0300

    last

--- wolfhead/js/main.js
+++ wolfhead/js/main.js
@@ -1,4 +1,9 @@
+'use strict';
 
+import Swiper, { Navigation, Pagination } from 'swiper';
+Swiper.use([Navigation, Pagination]);
+
+// Burger-menu for desctop and mobile
 const menuToggle = document.querySelector('#menu-toggle');
 const menuHide = document.querySelector('#menu-hide');
 const menuLogo = document.querySelector('#logo');
@@ -8,3 +13,16 @@ menuToggle.onclick = function () {
 	menuHide.classList.toggle('menu--hide');
 	menuLogo.classList.toggle('header__logo--left');
 }
+
+
+// Slider Swiper in reviews section 
+const mySwiper = new Swiper('.swiper-container', {
+	// Optional parameters
+	loop: true,
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+})
