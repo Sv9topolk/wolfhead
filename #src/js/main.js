e89e8be6b99fba240a5dcac087a561c6dde78ffc
Sv9topolk <sv9topolk@mail.ru>
2020-11-10 21:38:38 +0300

    work at mobile menu

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,18 +1,34 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
-Swiper.use([Navigation, Pagination]);
+import Swiper, { Navigation } from 'swiper';
+Swiper.use([Navigation]);
 
-// Burger-menu for desctop and mobile
+// ! Burger-menu for desctop and mobile
 const menuToggle = document.querySelector('#menu-toggle');
 const menuHide = document.querySelector('#menu-hide');
 const menuLogo = document.querySelector('#logo');
 
-menuToggle.onclick = function () {
+menuToggle.addEventListener('click', function () {
+	let body = document.querySelector('body');
+	let html = document.documentElement.clientWidth;
+	let bodyPadding = window.innerWidth - html;
+
 	menuToggle.classList.toggle('burger--active');
 	menuHide.classList.toggle('menu--hide');
 	menuLogo.classList.toggle('header__logo--left');
-}
 
-// Slider swiper for reviews section
+	if (window.matchMedia('(max-width: 768px)').matches && menuHide.classList.contains('menu--hide')) {
+		document.querySelector('.header').style.paddingRight = bodyPadding + 'px';
+		body.style.overflow = 'hidden';
+		menuHide.style.right = '-' + (window.innerWidth - html) + 'px';
+	} else {
+		document.querySelector('.header').style.paddingRight = 0 + 'px';
+		body.style.overflow = 'visible';
+		menuHide.style.right = '0';
+	}
+});
+
+
+
+// ! Slider swiper for reviews section
 const mySwiper = new Swiper('.swiper-container', {
 	// Optional parameters
 	loop: true,
