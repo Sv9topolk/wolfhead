9c38bfec28afec036883a2b551468d4614c2fb8c
Sv9topolk <sv9topolk@mail.ru>
2020-11-15 17:12:52 +0300

    add lazy anchor scroll

--- #src/js/main.js
+++ #src/js/main.js
@@ -97,3 +97,19 @@ document.querySelectorAll('.tabs-triggers__item').forEach((item) =>
 	})
 );
 
+// ! Menu lazy anchor scroll
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault()
+
+		const blockID = anchor.getAttribute('href').substr(1)
+
+		document.getElementById(blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+		})
+	})
+}
+
